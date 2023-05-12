e49b9c3b6f2c4d40643e586b4a6903117248429a
Kyle <prophet@hanyang.ac.kr>
2023-05-12 16:51:35 +0900

    DTO, Interface, Service

--- src/controller/GogakService.java
+++ src/controller/GogakService.java
@@ -1,6 +1,23 @@
 package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface GogakService {
 
+    void connect(String dsn, String uid, String pwd) throws SQLException;
 
+    void close();
+
+    void menu() throws IOException, SQLException;
+
+    void selectAll(String className) throws SQLException, IOException;
+
+    void insert(String className) throws IOException;
+
+    void update();
+
+    void delete(String className) throws IOException;
+
+    void selectByGno(String className) throws SQLException, IOException;
 }
--- src/controller/GogakServiceImpl.java
+++ src/controller/GogakServiceImpl.java
@@ -1,6 +1,59 @@
 package controller;
 
+import java.io.*;
+import java.sql.*;
+
 public class GogakServiceImpl implements GogakService {
 
+    private static final GogakService INSTANCE = new GogakServiceImpl();
+    static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Connection conn = null;
+    static Statement stmt = null;
+    static PreparedStatement pstmt = null;
+    static ResultSet rs = null;
 
+    private static GogakService getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void connect(String dsn, String uid, String pwd) throws SQLException {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void menu() throws IOException, SQLException {
+
+    }
+
+    @Override
+    public void selectAll(String className) throws SQLException, IOException {
+
+    }
+
+    @Override
+    public void insert(String className) throws IOException {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete(String className) throws IOException {
+
+    }
+
+    @Override
+    public void selectByGno(String className) throws SQLException, IOException {
+
+    }
 }
